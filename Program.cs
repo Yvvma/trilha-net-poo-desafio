a7049ca210081e4f52fd0701b40de0008f124348
Yudi <flvckopxdin@gmail.com>
2024-01-05 09:01:54 -0300

    modified:   Models/Iphone.cs 	modified:   Models/Nokia.cs 	modified:   Models/Smartphone.cs 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(Numero: "728198", Modelo: "Model 1", IMEI: "111111111111111", Memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone(Numero: "1234", Modelo: "Model 2", IMEI: "222222222222222", Memoria: 128);
+iphone.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
